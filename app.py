d65e2ccc5d0a8c994bd5282217be0472bdb7e7a2
Marina Georgati <marinag@plan.aau.dk>
2022-03-24 11:36:43 +0100

    completed app

--- app.py
+++ app.py
@@ -6,3 +6,38 @@ app = Flask(__name__)
 def root():
     return app.send_static_file('map.html')
 
+@app.route('/parking')
+def hello_park():
+    lat = float(request.args.get("lat", 54))
+    lng = float(request.args.get("lng", 10))
+    
+    # Connect to an existing database
+    conn = psycopg2.connect(dbname = "parking",
+        user = "postgres",
+        host = "localhost",
+        port = '5432',
+        password = "postgres",
+        sslmode="disable")
+    # Open a cursor to perform database operations
+    cur = conn.cursor()
+    # Query the database and obtain data as Python objects
+    cur.execute("""SELECT id, streetname, ST_AsGeoJSON(geometry)
+                            FROM phus
+                            ORDER BY geometry <-> 'SRID=4326;POINT(%s %s)'::geometry
+                            LIMIT 1;""", (lng, lat))
+    row = cur.fetchone()
+    output = '''{
+        "type": "Feature",
+        "properties": {
+            "id": '''+str(row[0])+''',
+            "streetname": "'''+row[1]+'''"},
+        "geometry": '''+ row[2]+ '''
+    }'''
+
+    cur.close()
+    conn.close()
+
+    return Response(response=output,
+                    status=200,
+                    mimetype="application/json")
+
