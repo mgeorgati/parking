3959449e9fd33329f3958c191005320b4c0cde12
Marina Georgati <marinag@plan.aau.dk>
2022-03-24 10:06:05 +0100

    initial commit

---
+++ db.py
@@ -0,0 +1,43 @@
+# import Python's built-in JSON library
+import json
+# import the psycopg2 database adapter for PostgreSQL
+import psycopg2
+from psycopg2 import Error
+try:
+    # Connect to an existing database
+    connection = psycopg2.connect(dbname = "parking",
+        user = "postgres",
+        host = "localhost",
+        port = '5432',
+        password = "postgres",
+        sslmode="disable")
+    # Create a cursor to perform database operations
+    cursor = connection.cursor()
+    # Print PostgreSQL details
+    print("PostgreSQL server information")
+    print(connection.get_dsn_parameters(), "\n")
+    # Executing a SQL query
+    cursor.execute("SELECT version();")
+    # Fetch result
+    record = cursor.fetchone()
+    print("You are connected to - ", record, "\n")
+    
+    # use Python's open() function to load the JSON data
+    with open('phus.json') as json_data:
+        # use load() rather than loads() for JSON files
+        df = json.load(json_data)
+    
+    print(df)
+    cursor.execute('DROP TABLE if exists phus;')
+    connection.commit()
+    cursor.execute('CREATE TABLE phus (id int, code varchar, streetname varchar, \
+        bemaerkning varchar, geometry geometry);')
+    
+
+except (Exception, Error) as error:
+    print("Error while connecting to PostgreSQL", error)
+finally:
+    if (connection):
+        cursor.close()
+        connection.close()
+        print("PostgreSQL connection is closed")
